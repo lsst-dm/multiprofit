8f3253e42eb0d9b454fb85efbb6c6ba8f7db965b
taranu <dtaranu@astro.princeton.edu>
2019-02-21 14:27:28 -0600

    Move more general-purpose functions into utils

--- multiprofit/utils.py
+++ multiprofit/utils.py
@@ -1,32 +1,8 @@
 import numpy as np
 
 
-def getchisqred(chis):
-    chisum = 0
-    chicount = 0
-    for chivals in chis:
-        chisum += np.sum(chivals**2)
-        chicount += len(chivals)**2
-    return chisum/chicount
-
-
-def fluxtomag(x):
-    return -2.5*np.log10(x)
-
-
-def magtoflux(x):
-    return 10**(-0.4*x)
-
-
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise ValueError('Cannot parse {} as boolean.'.format(v))
-
-
+# Remove negative elements in an array and preserve the sum by zeroing the smallest positive elements
+# This is a crude method of removing noise and returning a strictly positive image with the same total flux
 def absconservetotal(ndarray):
     shape = ndarray.shape
     ndarray.shape = np.prod(shape)
@@ -48,3 +24,45 @@ def absconservetotal(ndarray):
             raise RuntimeError("absconservetotal failed for array with sum {}".format(np.sum(ndarray)))
     ndarray.shape = shape
     return ndarray
+
+
+# https://stackoverflow.com/questions/3844801/check-if-all-elements-in-a-list-are-identical
+def allequal(iterator):
+    iterator = iter(iterator)
+    try:
+        first = next(iterator)
+    except StopIteration:
+        return True
+    return all(first == rest for rest in iterator)
+
+
+def getchisqred(chis):
+    chisum = 0
+    chicount = 0
+    for chivals in chis:
+        chisum += np.sum(chivals**2)
+        chicount += len(chivals)**2
+    return chisum/chicount
+
+
+def fluxtomag(x):
+    return -2.5*np.log10(x)
+
+
+def magtoflux(x):
+    return 10**(-0.4*x)
+
+
+def normalize(array):
+    array /= np.sum(array)
+    return array
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise ValueError('Cannot parse {} as boolean.'.format(v))
+
