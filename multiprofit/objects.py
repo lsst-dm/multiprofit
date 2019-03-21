Fix error in reduced chi squared calculation

multiprofit/objects.py
@@ -329,7 +329,7 @@ class Model:
         # axes[4].hist(chi[~np.isnan(chi)], bins=100, log=True, density=True, histtype="step", fill=False)
         x = np.linspace(-5., 5., int(1e4) + 1, endpoint=True)
         axes[4].plot(x, spstats.norm.pdf(x))
-        chisqred = np.sum(chi**2)/len(chi)
+        chisqred = np.sum(chi*chi)/len(chi)
         Model._labelfigureaxes(axes, chisqred, modelname=modelname, modeldesc=modeldesc,
                                labelimg=bandstring, isfirstmodel=isfirstmodel, islastmodel=islastmodel,
                                plotascolumn=plotascolumn,

multiprofit/utils.py
@@ -62,8 +62,8 @@ def getchisqred(chis):
     chisum = 0
     chicount = 0
     for chivals in chis:
-        chisum += np.sum(chivals**2)
-        chicount += len(chivals)
+        chisum += np.sum(chivals*chivals)
+        chicount += chivals.size
     return chisum/chicount
 
 