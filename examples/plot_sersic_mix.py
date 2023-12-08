acac4bf8433f58a75f5c6e6f25d934fc59dd9825
taranu <dan.s.taranu@gmail.com>
2023-12-08 16:01:50 -0500

    Add support for scipy interpolators in plot_sersicmix_interp

--- examples/plot_sersic_mix.py
+++ examples/plot_sersic_mix.py
@@ -2,14 +2,16 @@ import gauss2d.fit as g2f
 from lsst.multiprofit.plots import plot_sersicmix_interp
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import CubicSpline
 
 interps = {
-    'lin': (g2f.LinearSersicMixInterpolator(), (0, (5, 1))),
-    'g-csp': (g2f.GSLSersicMixInterpolator(interp_type=g2f.GSLInterpType.cspline), '-'),
+    "lin": (g2f.LinearSersicMixInterpolator(), "-"),
+    "gsl-csp": (g2f.GSLSersicMixInterpolator(interp_type=g2f.InterpType.cspline), (0, (8, 8))),
+    "scipy-csp": ((CubicSpline, {}), (0, (4, 4))),
 }
 
 for n_low, n_hi in ((0.5, 0.7), (2.2, 4.4)):
-    n_ser = 10**np.linspace(np.log10(n_low), np.log10(n_hi), 100)
+    n_ser = 10 ** np.linspace(np.log10(n_low), np.log10(n_hi), 100)
     plot_sersicmix_interp(interps=interps, n_ser=n_ser, figsize=(10, 8))
     plt.tight_layout()
     plt.show()
--- python/lsst/multiprofit/plots.py
+++ python/lsst/multiprofit/plots.py
@@ -7,6 +7,7 @@ import matplotlib as mpl
 import matplotlib.figure
 import matplotlib.pyplot as plt
 import numpy as np
+import typing
 
 from .utils import get_params_uniq
 
@@ -317,10 +318,11 @@ def plot_loglike(
     return fig, ax
 
 
+Interpolator: typing.TypeAlias = g2f.SersicMixInterpolator | tuple[typing.Type, dict[str, typing.Any]]
+
+
 def plot_sersicmix_interp(
-    interps: dict[str, tuple[g2f.SersicMixInterpolator, str | tuple]],
-    n_ser: np.ndarray,
-    **kwargs
+    interps: dict[str, tuple[Interpolator, str | tuple]], n_ser: np.ndarray, **kwargs
 ) -> matplotlib.figure.Figure:
     """Plot Gaussian mixture Sersic profile interpolated values.
 
@@ -337,16 +339,20 @@ def plot_sersicmix_interp(
     -------
         The resulting figure.
     """
-    orders = {name: interp.order for name, (interp, _)  in interps.items()}
+    orders = {
+        name: interp.order
+        for name, (interp, _) in interps.items()
+        if isinstance(interp, g2f.SersicMixInterpolator)
+    }
     order = set(orders.values())
     if not len(order) == 1:
         raise ValueError(f"len(set({orders})) != 1; all interpolators must have the same order")
     order = tuple(order)[0]
 
-    cmap = mpl.cm.get_cmap('tab20b')
-    colors_ord = [None]*order
+    cmap = mpl.cm.get_cmap("tab20b")
+    colors_ord = [None] * order
     for i_ord in range(order):
-        colors_ord[i_ord] = cmap(i_ord/(order - 1.))
+        colors_ord[i_ord] = cmap(i_ord / (order - 1.0))
 
     n_ser_min = np.min(n_ser)
     n_ser_max = np.max(n_ser)
@@ -378,39 +384,66 @@ def plot_sersicmix_interp(
     n_ser_knots = n_ser_knots[range_knots]
 
     n_values = len(n_ser)
-    integrals, dintegrals, sigmas, dsigmas = ({name: np.empty((n_values, order)) for name in interps}
-                                              for _ in range(4))
+    integrals, dintegrals, sigmas, dsigmas = (
+        {name: np.empty((n_values, order)) for name in interps} for _ in range(4)
+    )
+
+    for name, (interp, _) in interps.items():
+        if not isinstance(interp, g2f.SersicMixInterpolator):
+            kwargs = interp[1] if interp[1] is not None else {}
+            interp = interp[0]
+            x = [knot.sersicindex for knot in knots]
+            for i_ord in range(order):
+                integrals_i = np.empty(n_knots, dtype=float)
+                sigmas_i = np.empty(n_knots, dtype=float)
+                for i_knot, knot in enumerate(knots):
+                    integrals_i[i_knot] = knot.values[i_ord].integral
+                    sigmas_i[i_knot] = knot.values[i_ord].sigma
+                interp_int = interp(x, integrals_i, **kwargs)
+                dinterp_int = interp_int.derivative()
+                interp_sigma = interp(x, sigmas_i, **kwargs)
+                dinterp_sigma = interp_sigma.derivative()
+                for i_val, value in enumerate(n_ser):
+                    integrals[name][i_val, i_ord] = interp_int(value)
+                    sigmas[name][i_val, i_ord] = interp_sigma(value)
+                    dintegrals[name][i_val, i_ord] = dinterp_int(value)
+                    dsigmas[name][i_val, i_ord] = dinterp_sigma(value)
 
     for i_val, value in enumerate(n_ser):
         for name, (interp, _) in interps.items():
-            values = interp.integralsizes(value)
-            derivs = interp.integralsizes_derivs(value)
-            for i_ord in range(order):
-                integrals[name][i_val, i_ord] = values[i_ord].integral
-                sigmas[name][i_val, i_ord] = values[i_ord].sigma
-                dintegrals[name][i_val, i_ord] = derivs[i_ord].integral
-                dsigmas[name][i_val, i_ord] = derivs[i_ord].sigma
+            if isinstance(interp, g2f.SersicMixInterpolator):
+                values = interp.integralsizes(value)
+                derivs = interp.integralsizes_derivs(value)
+                for i_ord in range(order):
+                    integrals[name][i_val, i_ord] = values[i_ord].integral
+                    sigmas[name][i_val, i_ord] = values[i_ord].sigma
+                    dintegrals[name][i_val, i_ord] = derivs[i_ord].integral
+                    dsigmas[name][i_val, i_ord] = derivs[i_ord].sigma
 
     fig, axes = plt.subplots(2, 2, **kwargs)
     for idx_row, (yv, yd, yk, y_label) in (
-            (0, (integrals, dintegrals, integrals_knots, 'integral')),
-            (1, (sigmas, dsigmas, sigmas_knots, 'sigma'))
+        (0, (integrals, dintegrals, integrals_knots, "integral")),
+        (1, (sigmas, dsigmas, sigmas_knots, "sigma")),
     ):
         is_label_row = idx_row == 1
-        for idx_col, y_i, y_prefix in ((0, yv, ''), (1, yd, 'd')):
+        for idx_col, y_i, y_prefix in ((0, yv, ""), (1, yd, "d")):
             is_label_col = idx_col == 0
             make_label = is_label_col and is_label_row
             axis = axes[idx_row, idx_col]
             if is_label_col:
                 for i_ord in range(order):
-                    axis.plot(n_ser_knots, yk[:, i_ord], 'kx',
-                              label='knots' if make_label and (i_ord == 0) else None)
+                    axis.plot(
+                        n_ser_knots,
+                        yk[:, i_ord],
+                        "kx",
+                        label="knots" if make_label and (i_ord == 0) else None,
+                    )
             for name, (_, lstyle) in interps.items():
                 for i_ord in range(order):
                     label = f"{name}" if make_label and (i_ord == 0) else None
                     axis.plot(n_ser, y_i[name][:, i_ord], c=colors_ord[i_ord], label=label, linestyle=lstyle)
                 axis.set_xlim((n_ser_min, n_ser_max))
-                axis.set_ylabel(f'{y_prefix}{y_label}')
+                axis.set_ylabel(f"{y_prefix}{y_label}")
             if make_label:
                 axis.legend(loc="upper left")
     return fig
